ff065df685809646fe792b52efe31dc4a31937f6
HTY <1044213317@qq.com>
2022-03-11 19:16:46 -0800

    simulation code

--- controller_sim_ros/src/so3_control/src/linear_control.cpp
+++ controller_sim_ros/src/so3_control/src/linear_control.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <ros/ros.h>
 #include <math.h>
+#include <tf/tf.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
 
 LinearControl::LinearControl()
   : mass_(0.49)
@@ -29,6 +33,19 @@ LinearControl::calculateControl(const Desired_State_t &des,
                         Gain gain)
 {
     /* WRITE YOUR CODE HERE */
-  // u.q = ...
-  // u.thrust = ...
+    Eigen::Vector3d angle = odom.q.matrix().eulerAngles(2, 0, 1);
+
+    Eigen::Vector3d a_out;
+    a_out[0] = des.a[0] + gain.Kv0 * (des.v[0] - odom.v[0]) + gain.Kp0 * (des.p[0] - odom.p[0]);
+    a_out[1] = des.a[1] + gain.Kv1 * (des.v[1] - odom.v[1]) + gain.Kp1 * (des.p[1] - odom.p[1]);
+    a_out[2] = des.a[2] + gain.Kv2 * (des.v[2] - odom.v[2]) + gain.Kp2 * (des.p[2] - odom.p[2]);
+
+    u.thrust = LinearControl::mass_ * (LinearControl::g_ + a_out[2]);
+
+    double phi = 1 / LinearControl::g_ * (a_out[0] * sin(angle[0]) - a_out[1] * cos(angle[0]));
+    double theta = 1 / LinearControl::g_ * (a_out[0] * cos(angle[0]) + a_out[1] * sin(angle[0]));
+
+    u.q = Eigen::AngleAxisd(angle[0], Eigen::Vector3d::UnitZ()) *
+            Eigen::AngleAxisd(phi, Eigen::Vector3d::UnitX()) *
+            Eigen::AngleAxisd(theta, Eigen::Vector3d::UnitY());
 }
